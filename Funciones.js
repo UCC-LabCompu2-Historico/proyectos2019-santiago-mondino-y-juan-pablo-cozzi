33bb4fbcb7b4fa87bbe0ef9eb4ba4a48bbe80af8
juanpicozzi <juan.p.cozzi@hotmail.com>
2019-06-21 23:46:11 -0300

    Realiza calculo; dibuja triangulo(sin el texto en el canvas, no se logro agregar);se modifico la teoria

--- Funciones.js
+++ Funciones.js
@@ -1,6 +1,12 @@
 
+/**
+ * Esta funcion lo que hace es realizar el calculo de los datos faltantes del triangulo rectangulo que se desea obtener.
+ * Para ello primero analiza que datos ha ingresado el usuario y en base a eso, realiza el calculo correspondiente.
+ * @method Calcular
+ */
 
 function Calcular(){
+
     var Ladoa = document.getElementById("Valor1a").value;
     var Ladob = document.getElementById("Valor1b").value;
     var Ladoc = document.getElementById("Valor1c").value;
@@ -11,7 +17,8 @@ function Calcular(){
     var PerimTriang = document.getElementById("Perimetro").value;
 
 
-// FALTAN AGREGAR UN PAR DE COMBINACIONES PARA CUANDO TENEMOS DOS LADOS Y UN SOlo ANGULO.   
+
+// FALTAN AGREGAR UN PAR DE COMBINACIONES PARA CUANDO TENEMOS DOS LADOS Y UN SOlo ANGULO.
 
     if (Anguloβ == '' && Ladob=='' && Ladoc==''){
         var angbeta = (Anguloγ-Anguloα);
@@ -92,22 +99,123 @@ function Calcular(){
         var PerimetroT = parseInt(Ladoc) + (catetoB + catetoA) ;
         document.getElementById("Perimetro").value = PerimetroT;
     }
+    else if(Ladoa==''){
+        var catetoB = parseInt(Ladob);
+        var catetoC = parseInt(Ladoc);
+        var catetoA= Math.sqrt((Ladoc*Ladoc)-(Ladob*Ladob));
+        document.getElementById("Valor1a").value = catetoA;
+        var angalfa= Math.atan(catetoA/Ladob);
+        var ang_convertido = (angalfa*180)/(Math.PI);
+        document.getElementById("Valor1A").value = ang_convertido;
+        var angbeta = (Anguloγ-ang_convertido);
+        document.getElementById("Valor1B").value = angbeta;
 
+        var Area = ((Ladob*catetoA)/2);
+        document.getElementById("Area").value = Area;
+        var PerimetroT = parseInt(catetoB) + (catetoC + catetoA) ;
+        document.getElementById("Perimetro").value = PerimetroT;
+
+    }
+    else if(Ladob==''){
+        var catetoA = parseInt(Ladoa);
+        var catetoC = parseInt(Ladoc);
+        var catetoB= Math.sqrt((Ladoc*Ladoc)-(Ladoa*Ladoa));
+        document.getElementById("Valor1b").value = catetoB;
+        var angalfa= Math.atan(Ladoa/catetoB);
+        var ang_convertido = (angalfa*180)/(Math.PI);
+        document.getElementById("Valor1A").value = ang_convertido;
+        var angbeta = (Anguloγ-ang_convertido);
+        document.getElementById("Valor1B").value = angbeta;
+        var Area = ((Ladoa*catetoB)/2);
+        document.getElementById("Area").value = Area;
+        var PerimetroT = parseInt(catetoB) + (catetoC + catetoA) ;
+        document.getElementById("Perimetro").value = PerimetroT;
+
+    }
+    else if(Ladoc=='') {
+        var catetoA = parseInt(Ladoa);
+        var catetoB = parseInt(Ladob);
+        var catetoC = Math.sqrt((Ladoa * Ladoa) + (Ladob * Ladob));
+        document.getElementById("Valor1c").value = catetoC;
+        var angalfa = Math.atan(Ladoa / Ladob);
+        var ang_convertido = (angalfa * 180) / (Math.PI);
+        document.getElementById("Valor1A").value = ang_convertido;
+        var angbeta = (Anguloγ - ang_convertido);
+        document.getElementById("Valor1B").value = angbeta;
+        var Area = ((Ladob*Ladoa)/2);
+        document.getElementById("Area").value = Area;
+        var PerimetroT = parseInt(catetoB) + (catetoC + catetoA) ;
+        document.getElementById("Perimetro").value = PerimetroT;
+
+    }
+
+     if (Ladoa=='' && Ladob=='' && Ladoc==''){
+        alert("Por Favor, ingrese al menos un lado del triangulo");
+    }
 }
 
+
+/**
+ * La funcion aqui abajo realiza el dibujo del triangulo correspondiente a los datos ingresados/calculados.
+ * El dibujo se hace sobre un canvas
+ * @method dibujar
+ */
+function dibujar() {
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    var base = parseInt(document.getElementById("Valor1b").value);
+    var altura = parseInt(document.getElementById("Valor1a").value);
+
+    var Xmedia= canvas.width/2;
+    var Ymedia = canvas.height/2;
+    //context.scale(0.8,0.8);
+
+    context.beginPath();
+    context.moveTo(Xmedia, Ymedia);
+    context.lineTo(base, Ymedia);
+    context.lineTo(base, altura);
+    context.closePath();
+
+    //Contorno
+    context.lineWidth = 2;
+    context.strokeStyle = '#000000';
+    context.stroke();
+
+    //relleno
+    context.fillStyle = "#FFCC00";
+    context.fill();
+
+
+}
+/**
+ * La siguiente funcion lo que hace es reiniciar todos los inputs a su valor original, con el objetivo de que el usuario
+ * realice nuevamente un calculo.
+ * @method Reiniciar
+ */
+
 function Reiniciar() {
+
+
     document.getElementById("Valor1B").value = null;
-    var angalfa = " ";
     document.getElementById("Valor1A").value = null;
-    var catetoA = " ";
     document.getElementById("Valor1a").value = null;
-    var catetoC = " ";
     document.getElementById("Valor1c").value = null;
-    var catetoB = " ";
     document.getElementById("Valor1b").value = null;
-    var Area = " ";
     document.getElementById("Area").value = null;
-    var PerimetroT = " " ;
     document.getElementById("Perimetro").value = null;
+}
+
+/**
+ * La siguiente funcion lo que hace es limpiar el canvas, con el objetivo de que cuando el usuario
+ * realice nuevamente un calculo, se dibuje el triangulo correspondiente si tener al anterior por debajo.
+ * @method Limpiar
+ */
+function Limpiar() {
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    context.clearRect (0, 0, canvas.width, canvas.height);
+
 }
 
